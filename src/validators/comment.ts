e41f2265069ac6d85c0548ddeb926223cc32947a
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2025-05-28 12:48:32 +0400

    implement tests for comments

--- src/validators/comment.ts
+++ src/validators/comment.ts
@@ -1,7 +1,6 @@
 import { z } from "zod";
 
 export const createCommentSchema = z.object({
-  blogId: z.string().min(1),
   name: z.string().min(1),
   content: z.string().min(1),
   approved: z.boolean(),
